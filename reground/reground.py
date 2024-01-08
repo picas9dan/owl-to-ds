d043af576260d89c191ab87eebcc4973999270d8
Dan Tran <nmdt2@cam.ac.uk>
2024-01-08 15:21:36 +0800

    Update regrounding for ontokin

---
+++ reground/reground.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+import networkx as nx
+from typing import List, Tuple
+
+class Regrounder(ABC):
+    @abstractmethod
+    def reground(self, query_graph: nx.DiGraph, query_sparql: str, paraphrases: List[str]) -> List[Tuple[str, str]]:
+        pass
