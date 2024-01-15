0cee5c0fa0d4b406deb5191513a9509f5f7859e0
Dan Tran <nmdt2@cam.ac.uk>
2024-01-15 11:28:45 +0800

    Gather chemclasses for synthetic ABox generation

--- locate_then_ask/ontospecies/synthetic/__init__.py
+++ locate_then_ask/ontospecies/synthetic/__init__.py
@@ -1,4 +1,5 @@
 from locate_then_ask.ontospecies.model import OSSpecies
+from .chemclass import OSChemClassSynthesizer
 from .property import OSPropertySynthesizer
 from .identifier import OSIdentifierSynthesizer
 
@@ -7,12 +8,13 @@ class OSSpeciesSynthesizer:
     def __init__(self):
         self.prop_synth = OSPropertySynthesizer()
         self.ident_synth = OSIdentifierSynthesizer()
+        self.chemclass_synth = OSChemClassSynthesizer()
 
     def make(self):
         return OSSpecies(
             iri="synthetic_entity",
             key2identifier=self.prop_synth.make(),
             key2property=self.ident_synth.make(),
-            chemclasses=None,
+            chemclasses=self.chemclass_synth.make(),
             uses=None,
         )
